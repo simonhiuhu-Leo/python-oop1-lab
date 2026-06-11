c40c5520e69728f045a6905cfd83ee78c15e6270
simonhiuhu-Leo <simon.hiuhu@student.moringaschool.com>
2026-06-11 23:11:48 +0300

    Finished lab

--- lib/book.py
+++ lib/book.py
@@ -1,15 +1,20 @@
-class Book:
-    def init(self,title = None,page_count = 0) :
-        self.title = title
-        self._page_count = page_count
+#!/usr/bin/env python3
 
-    def get_page_count(self):
+class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
         return self._page_count
-    def set_page_count(self,value):
-        if isinstance(value,int):
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
             self._page_count = value
         else:
             print("page_count must be an integer")
-    def turn_page(self) : 
+
+    def turn_page(self):
         print("Flipping the page...wow, you read fast!")
-    page_count = property(get_page_count,set_page_count)
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,15 +1,19 @@
 class Coffee:
-    def init(self,size,price):
-        self._size = size
+    def __init__(self, size, price):
+        self.size = size
         self.price = price
-    def get_size(self):
+
+    @property
+    def size(self):
         return self._size
-    def set_size(self,value):
-        if value != "Small" or value != "Medium" or value != "Large" :
-            print("size must be Small, Medium, or Large") 
-        else:
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
             self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
     def tip(self):
         print("This coffee is great, here’s a tip!")
-        self.price += 1 
-    size = property(get_size,set_size)
+        self.price += 1
